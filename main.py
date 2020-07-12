e73f22728566232403db4ebe9c1d06ef37feae5b
Francis Tseng <f+accounts@frnsys.com>
2020-07-12 10:07:23 -0400

    reduce memory usage by processing tweets per user rather than all at once

--- main.py
+++ main.py
@@ -10,10 +10,6 @@ from metadata import get_metadata
 from feedgen.feed import FeedGenerator
 from apscheduler.schedulers.blocking import BlockingScheduler
 
-auth = tweepy.OAuthHandler(config.CONSUMER_KEY, config.CONSUMER_SECRET)
-auth.set_access_token(config.ACCESS_TOKEN, config.ACCESS_TOKEN_SECRET)
-api = tweepy.API(auth)
-
 logging.basicConfig(level=logging.INFO,
                     format='%(asctime)s:%(levelname)s:%(message)s',
                     datefmt='%Y-%m-%d %H:%M:%S %Z')
@@ -23,6 +19,10 @@ logger.setLevel(logging.INFO)
 
 def main():
     logger.info('Running...')
+
+    auth = tweepy.OAuthHandler(config.CONSUMER_KEY, config.CONSUMER_SECRET)
+    auth.set_access_token(config.ACCESS_TOKEN, config.ACCESS_TOKEN_SECRET)
+    api = tweepy.API(auth)
     db = Database('data/db')
 
     now = datetime.now().timestamp()
@@ -39,52 +39,50 @@ def main():
     users = sorted(list(users), key=lambda u: last_seen.get(u, -1))
     logger.info('{} users'.format(len(users)))
 
-    tweets = []
+    metadata = {}
     try:
         for user in users:
             last = last_seen.get(user, None)
-            tweets += api.user_timeline(user_id=user, count=200, since_id=last)
+            tweets = api.user_timeline(user_id=user, count=200, since_id=last)
+            for t in tweets:
+                user = t.user.screen_name
+                user_id = t.user.id
+
+                urls = t.entities['urls']
+                if hasattr(t, 'retweeted_status'):
+                    urls += t.retweeted_status.entities['urls']
+                if hasattr(t, 'quoted_status'):
+                    urls += t.quoted_status.entities['urls']
+
+                urls = [url['expanded_url'] for url in urls]
+                for url in set(urls):
+                    if util.is_twitter_url(url): continue
+
+                    try:
+                        if url not in metadata:
+                            metadata[url] = get_metadata(url)
+                        meta = metadata[url]
+                    except Exception as e:
+                        logger.info('Error getting metadata for {}: {}'.format(url, e))
+                        meta = {'url': url}
+
+                    url = meta['url']
+                    if util.is_twitter_url(url): continue
+
+                    logger.info('@{}: {}'.format(user, url))
+                    db.inc(url, user)
+
+                last = last_seen.get(user_id, None)
+                last_updated[user_id] = now
+                if last is None or t.id > last: last_seen[user_id] = t.id
+
+            with open('data/last_seen', 'w') as f:
+                json.dump(last_seen, f)
+            with open('data/last_updated', 'w') as f:
+                json.dump(last_updated, f)
+
     except tweepy.error.RateLimitError:
         logger.info('Rate limited')
-    logger.info('{} new tweets'.format(len(tweets)))
-
-    metadata = {}
-    for t in tweets:
-        user = t.user.screen_name
-        user_id = t.user.id
-
-        urls = t.entities['urls']
-        if hasattr(t, 'retweeted_status'):
-            urls += t.retweeted_status.entities['urls']
-        if hasattr(t, 'quoted_status'):
-            urls += t.quoted_status.entities['urls']
-
-        urls = [url['expanded_url'] for url in urls]
-        for url in set(urls):
-            if util.is_twitter_url(url): continue
-
-            try:
-                if url not in metadata:
-                    metadata[url] = get_metadata(url)
-                meta = metadata[url]
-            except Exception as e:
-                logger.info('Error getting metadata for {}: {}'.format(url, e))
-                meta = {'url': url}
-
-            url = meta['url']
-            if util.is_twitter_url(url): continue
-
-            logger.info('@{}: {}'.format(user, url))
-            db.inc(url, user)
-
-        last = last_seen.get(user_id, None)
-        last_updated[user_id] = now
-        if last is None or t.id > last: last_seen[user_id] = t.id
-
-    with open('data/last_seen', 'w') as f:
-        json.dump(last_seen, f)
-    with open('data/last_updated', 'w') as f:
-        json.dump(last_updated, f)
 
     # Compile RSS
     fg = FeedGenerator()
